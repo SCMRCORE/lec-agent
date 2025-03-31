c921465a1752934b2f5f2675292593967a03ad35
SCMRCORE <3551147139@qq.com>
2025-03-31 20:00:53 +0800

    init

---
+++ src/main/java/com/example/lecagent/LecAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.lecagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LecAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lecagent/LecAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
