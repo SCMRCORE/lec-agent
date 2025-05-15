4df7a562e6cbe22a885a505b068e026f8f09191d
SCMRCORE <3551147139@qq.com>
2025-05-15 21:34:12 +0800

    update:try to fix mcp

--- src/main/java/com/example/lecagent/LecAgentApplication.java
+++ src/main/java/com/example/lecagent/LecAgentApplication.java
@@ -14,4 +14,28 @@ public class LecAgentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LecAgentApplication.class, args);
 	}
+
+//	@Bean
+//	public CommandLineRunner predefinedQuestions(
+//			ChatClient.Builder chatClientBuilder,
+//			ToolCallbackProvider tools,
+//			ConfigurableApplicationContext context) {
+//		return args -> {
+//			// 构建ChatClient并注入MCP工具
+//			var chatClient = chatClientBuilder
+//					.defaultTools(tools)
+//					.build();
+//
+//			// 定义用户输入
+//			String userInput = "用高德地图查询北京今天的天气";
+//			// 打印问题
+//			System.out.println("\n>>> QUESTION: " + userInput);
+//			// 调用LLM并打印响应
+//			System.out.println("\n>>> ASSISTANT: " +
+//					chatClient.prompt(userInput).call().content());
+//
+//			// 关闭应用上下文
+//			context.close();
+//		};
+//	}
 }
